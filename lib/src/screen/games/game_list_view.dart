529baee055f7a7784b7eddd5de00fca39c7788b1
89pleasure <133201120+89pleasure@users.noreply.github.com>
2023-06-17 12:11:01 +0200

    Fix: Reset game version filter on game change

--- lib/src/screen/games/game_list_view.dart
+++ lib/src/screen/games/game_list_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:modmopet/src/entity/emulator.dart';
 import 'package:modmopet/src/entity/game.dart';
+import 'package:modmopet/src/entity/mod.dart';
 import 'package:modmopet/src/provider/game_list_provider.dart';
 import 'package:modmopet/src/screen/games/game_list_actionbar_widget.dart';
 import 'package:modmopet/src/screen/games/game_list_empty_view.dart';
@@ -110,6 +111,7 @@ class GameListView extends HookConsumerWidget {
               ),
               onTap: () {
                 ref.watch(gameProvider.notifier).state = game;
+                ref.watch(gameVersionsProvider.notifier).state = {};
                 Navigator.restorablePushNamed(
                   context,
                   ModsView.routeName,
